f6ad8bbfa36cb464497b5be8de86652d09debf29
Mukhtar <mukhtaaramiin@gmail.com>
2022-09-11 16:00:18 +0300

    fixed hash password comparing

--- passportConfig.js
+++ passportConfig.js
@@ -1,30 +1,34 @@
-const  { Strategy } = require("passport-local");
+const { Strategy } = require("passport-local");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 
-
 //-------------passport configuration------------------//
-// a middleware that takes the user password and compare it to the encrypted password in the db using bcrypt 
+// a middleware that takes the user password and compare it to the encrypted password in the db using bcrypt
+const compare = async (hash, pass) => {
+  return bcrypt.compare(hash, pass);
+};
+
 const passportConfig = async (passport) => {
   passport.use(
     new Strategy((username, password, done) => {
       const user = prisma.users
         .findFirst({ where: { username } })
-        .then((user) => {
+        .then(async (user) => {
           if (!user) {
             return done(null, false, {
               message: "invalid username or password",
             });
           }
 
-          const isValid = bcrypt.compare(password, user.password);
+          const isValid = await compare(password, user.password);
 
           if (isValid) {
             return done(null, user);
           } else {
             return done(null, false, {
               message: "invalid username or password",
+              status: 401,
             });
           }
         })
